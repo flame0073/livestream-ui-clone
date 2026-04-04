b2e7ede3d7138bb0686e5a8345957e1415df700d
flame0073 <fflame143@gmail.com>
2026-04-05 07:49:34 +0800

    Disable CommentSection import and usage in Watch.tsx

---
+++ src/components/Watch.tsx
@@ -0,0 +1,15 @@
+// CommentSection import and usage have been disabled.
+
+// import CommentSection from './CommentSection';
+
+const Watch = () => {
+    return (
+        <div>
+            {/* CommentSection */}
+            {/* <CommentSection /> */}
+            <h1>Watch Component</h1>
+        </div>
+    );
+};
+
+export default Watch;
